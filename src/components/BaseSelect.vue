a18c905ce0b84404d1348ad33777b28ca02e0d59
mohamed <ragabmohame6@gmail.com>
2021-09-21 17:36:35 +0200

    Form validation

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <label v-if="label">{{ label }}</label>
-    <select :value="value" @input="updateValue" v-bind="$attrs">
-      <option v-for="option in options" :key="option">{{ option }}</option>
+    <select
+      :value="value"
+      @change="updateValue"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      <option
+        v-for="option in options"
+        :key="option"
+        :selected="option === value"
+      >
+        {{ option }}
+      </option>
     </select>
   </div>
 </template>
