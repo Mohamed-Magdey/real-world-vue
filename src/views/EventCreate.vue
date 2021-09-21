3d2a026edf8f85480f560dba45253c11b6c59721
mohamed <ragabmohame6@gmail.com>
2021-09-21 13:32:36 +0200

    Refactor using BaseInput

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -7,31 +7,31 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        label="Title"
+        v-model="event.title"
+        placeholder="Title"
+        class="field"
+      />
+
+      <BaseInput
+        type="text"
+        label="Description"
+        v-model="event.description"
+        placeholder="Description"
+        class="field"
+      />
+
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        label="Location"
+        v-model="event.location"
+        placeholder="Location"
+        class="field"
+      />
+
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
