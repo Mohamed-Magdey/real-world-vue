e74da737c2666dcdf5e47fbdd3bb5009b28d35c6
mohamed <ragabmohame6@gmail.com>
2021-09-22 21:02:37 +0200

    Handle router for login & register

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/"> Home </router-link>
     <router-link to="/dashboard"> Dashboard </router-link>
+    <router-link to="/login" class="button"> Login </router-link>
   </div>
 </template>
 
